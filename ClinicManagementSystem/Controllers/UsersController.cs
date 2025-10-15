1f037b221dd6208e9e1dc4ec659235fcb7567b97
Sakr <msakr364@gmail.com>
2025-10-16 00:44:41 +0300

    add user controller

---
+++ ClinicManagementSystem/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicManagementSystem.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
